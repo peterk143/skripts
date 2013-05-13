439ed9b2c08cabb87c4883251f772448f307b1b9
Peter Kirkpatrick <peter.k143@gmail.com>
2013-05-12 23:06:52 -0400

    broke rsync :/

--- updateShit.sh
+++ updateShit.sh
@@ -1,46 +1,56 @@
 #! /bin/bash
 
-MACHINES="dmzshell001
-dmzshell002
-dmzshell003
-dmzshell004
-dmzlegacyshell001
-dmzlegacyshell002
-nagios001
-nagios002
-tnode001"
+MACHINES="dmzshell001"
+# dmzshell002
+# dmzshell003
+# dmzshell004
+# dmzlegacyshell001
+# dmzlegacyshell002
+# nagios001
+# nagios002
+# tnode001"
 
-FILES="/home/pkirkpat/.bashrc
-/cloudhome/pkirkpat/.bash_aliases
-/home/pkirkpat/.ssh/authorized_keys
-/home/pkirkpat/.ssh/config"
+# file prep
+mkdir -p ~/temp
+cp ~/.ssh/{authorized_keys,config} /cloudhome/pkirkpat/.bash_aliases ~/temp
+tar cvzf ~/temp.tar.gz ~/temp/*
 
 RSYNC_OPTS="rsync -avz --progress"
+TEMP="~/temp.tar.gz"
+# ssh key check
 CHECK=`ssh-add -l`
-
 if [ $? -eq 0 ]
 then
+    # file transpo
     for host in ${MACHINES}
     do
 	echo ${host}
-	for each in ${FILES}
-	do
-	    case "$host" in
-		dmzshell*) ${RSYNC_OPTS} -e "ssh -p 20110" ${each} ${host}.lcsee.wvu.edu:~/ ;;
-		*) ${RSYNC_OPTS} ${each} ${host}.lcsee.wvu.edu:~/ ;;
-	    esac
-	done
+	case "$host" in
+	    dmzshell*) ${RSYNC_OPTS} -e "ssh -p 20110" ${TEMP} ${host}.lcsee.wvu.edu:~/ ;;
+	    *) ${RSYNC_OPTS} ${TEMP} ${host}.lcsee.wvu.edu:~/ ;;
+	esac
     done
 
-    SSH_OPTS="ssh ${h}"
-    RMT_CMD="mkdir -p ~/.ssh && mv ~/authorized_keys ~/config ~/.ssh"
+    # put files into place
+    RMT_CMD="tar xzvf ~/temp.tar.gz && mkdir -p ~/.ssh && mv ~/temp/authorized_keys ~/temp/config ~/.ssh"
     for h in ${MACHINES}
     do
 	case "$h" in
 	    dmzshell*) ssh -p 20110 ${h} "${RMT_CMD}" ;;
-	    *) ssh ${h} ${RMT_CMD} ;;
+	    *) ssh ${h} "${RMT_CMD}" ;;
 	esac
     done
+
+    # clean up temps
+    CLEAN="rm -rf ~/temp && rm ~/temp.tar.gz"
+    for x in ${MACHINES}
+    do
+	case "$x" in
+	    dmzshell*) ssh -p 20110 ${x} "${CLEAN}" ;;
+	    *) ssh ${x} "${CLEAN}" ;;
+	esac
+    done
+    eval ${CLEAN}
 
     echo "allGood!"
 else
